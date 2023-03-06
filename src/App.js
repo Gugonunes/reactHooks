6d84a7f2463b5e7a1eb3cc724a47ed746ddee1f3
Gugonunes <gugonunes@hotmail.com>
2023-03-06 17:46:09 -0300

    project initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+export default function App() {
+  const [repositories, setRepositories] = useState([
+    { id: 1, name: 'repo-1'},
+    { id: 2, name: 'repo-2'},
+    { id: 3, name: 'repo-3'}
+  ])
+
+  function handleAddRepository() {
+    setRepositories([...repositories,
+      { id: Math.random(), name: 'Novo repo'}
+    ]);
+  }
+
+  return (
+    <>
+      <ul>
+        {repositories.map(repo => (
+          <li key={repo.id}>{repo.name}</li>
+        ))}
+      </ul>
+      <button onClick={handleAddRepository}>
+        Add repo
+      </button>
+    </>
+  );
+}
+
