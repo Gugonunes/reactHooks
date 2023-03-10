0da4807a46c12370e73d33ad39f40f76ff359772
gugonunes <gugonunes@hotmail.com>
2023-03-09 22:55:48 -0300

    add favorites and page title with counter

--- src/App.js
+++ src/App.js
@@ -12,11 +12,30 @@ export default function App() {
     fetchData()
   }, []);
 
+  useEffect(() => {
+    const filtered = repositories.filter(repo => repo.favorite);
+
+    document.title = `Você tem ${filtered.length} favoritos`;
+
+  }, [repositories]);
+
+  function handleFavorite(id) {
+    const newRepositories = repositories.map(repo => {
+      return repo.id === id ? { ...repo, favorite: !repo.favorite } : repo
+    });
+
+    setRepositories(newRepositories);
+  }
+
   return (
     <>
       <ul>
         {repositories.map(repo => (
-          <li key={repo.id}>{repo.name}</li>
+          <li key={repo.id}>
+            {repo.name}
+            {repo.favorite && <span>(Favorito)</span>}
+            <button onClick={() => handleFavorite(repo.id)}>Favoritar</button>
+          </li>
         ))}
       </ul>
     </>
